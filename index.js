c24fd92e755d60540a193564cd9050d5a165d56a
slot181 <1253403835@qq.com>
2025-02-12 14:37:27 +0800

    index.js修改

--- index.js
+++ index.js
@@ -8,26 +8,39 @@ if (!global.segment) {
 
 let ret = [];
 
-logger.info(logger.yellow("- 正在载入 siliconflow-PLUGIN"));
+logger.info(logger.yellow("- 正在载入 lingyu-plugin"));
 
+// 使用绝对路径
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const baseDir = path.resolve(__dirname, './plugins/siliconflow-plugin');
+const baseDir = path.resolve(__dirname, './plugins/LingYu-plugin');
 const appsPath = path.join(baseDir, 'apps');
+// const modulesPath = path.join(baseDir, 'apps/modules'); // 注释掉或删除此行
 
-const files = fs
-  .readdirSync(appsPath)
-  .filter((file) => file.endsWith('.js'));
+const readJsFiles = (dirPath) => {
+  if (!fs.existsSync(dirPath)) {
+    logger.warn(`路径不存在: ${dirPath}`);
+    return [];
+  }
+  return fs.readdirSync(dirPath)
+    .filter((file) => file.endsWith('.js'))
+    .map((file) => path.join(dirPath, file));
+};
 
-files.forEach((file) => {
-  ret.push(import(path.join(appsPath, file)));
+const appsFiles = readJsFiles(appsPath);
+// const modulesFiles = readJsFiles(modulesPath); // 注释掉或删除此行
+
+const allFiles = [...appsFiles]; // 仅包含 appsFiles
+
+allFiles.forEach((filePath) => {
+  ret.push(import(filePath));
 });
 
 ret = await Promise.allSettled(ret);
 
 let apps = {};
-for (let i in files) {
-  let name = files[i].replace('.js', '');
+for (let i in allFiles) {
+  let name = path.basename(allFiles[i], '.js');
 
   if (ret[i].status !== 'fulfilled') {
     logger.error(`载入插件错误：${logger.red(name)}`);
@@ -37,6 +50,6 @@ for (let i in files) {
   apps[name] = ret[i].value[Object.keys(ret[i].value)[0]];
 }
 
-logger.info(logger.green("- siliconflow-PLUGIN 载入成功"));
+logger.info(logger.green("- lingyu-plugin 载入成功"));
 
 export { apps };
